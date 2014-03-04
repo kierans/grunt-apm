1f0915ce5de9e0877849012b30fcb1ffc8ff42db
Aymeric Beaumet <aymeric@beaumet.me>
2014-03-04 01:41:04 +0000

    Add the npm-test task

--- tasks/apm.js
+++ tasks/apm.js
@@ -12,12 +12,25 @@ module.exports = function (grunt) {
 
   var exec = require('child_process').exec;
 
+  var afterExec = function(err, stdout, stderr, done) {
+    var hasFailed = (err && err.code !== 0);
+    console.log(hasFailed ? stderr : stdout);
+    done(hasFailed);
+  };
+
   grunt.registerTask('apm-link', 'Create a symlink for the package in ~/.atom/packages.', function() {
     var done = this.async();
 
     exec('apm link .', function(err, stdout, stderr) {
-      console.log((err) ? stderr : stdout);
-      done(!err || err.code === 0);
+      afterExec(err, stdout, stderr, done);
+    });
+  });
+
+  grunt.registerTask('apm-test', 'Runs the package\'s tests contained within the spec directory.', function() {
+    var done = this.async();
+
+    exec('apm test', function(err, stdout, stderr) {
+      afterExec(err, stdout, stderr, done);
     });
   });
 
@@ -25,8 +38,7 @@ module.exports = function (grunt) {
     var done = this.async();
 
     exec('apm unlink .', function(err, stdout, stderr) {
-      console.log((err) ? stderr : stdout);
-      done(!err || err.code === 0);
+      afterExec(err, stdout, stderr, done);
     });
   });
 
