ae0835423249817fbcb86730d4872001c6896fa3
Aymeric Beaumet <aymeric@beaumet.me>
2014-03-04 01:16:57 +0000

    Add the apm-link and apm-unlink tasks

--- tasks/apm.js
+++ tasks/apm.js
@@ -10,41 +10,23 @@
 
 module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var exec = require('child_process').exec;
 
-  grunt.registerMultiTask('apm', 'An easy way to deal with APM from Grunt.', function () {
+  grunt.registerTask('apm-link', 'Create a symlink for the package in ~/.atom/packages.', function() {
+    var done = this.async();
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    exec('apm link .', function(err, stdout, stderr) {
+      console.log((err) ? stderr : stdout);
+      done(!err || err.code === 0);
     });
+  });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+  grunt.registerTask('apm-unlink', 'Delete the symlink in ~/.atom/packages for the package.', function() {
+    var done = this.async();
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
+    exec('apm unlink .', function(err, stdout, stderr) {
+      console.log((err) ? stderr : stdout);
+      done(!err || err.code === 0);
     });
   });
 
