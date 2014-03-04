aede7f7e788518c4f5d178c4b41d5029ab254089
Aymeric Beaumet <aymeric@beaumet.me>
2014-03-04 13:29:02 +0000

    done() is now called with the right parameter

--- tasks/apm.js
+++ tasks/apm.js
@@ -12,17 +12,17 @@ module.exports = function (grunt) {
 
   var exec = require('child_process').exec;
 
-  var afterExec = function(err, stdout, stderr, done) {
-    var hasFailed = (err && err.code !== 0);
-    console.log(hasFailed ? stderr : stdout);
-    done(hasFailed);
+  var afterExec = function(err, stdout, stderr) {
+    var hasSucceed = !(err && err.code !== 0);
+    console.log(hasSucceed ? stdout : stderr);
+    return hasSucceed;
   };
 
   grunt.registerTask('apm-link', 'Create a symlink for the package in ~/.atom/packages.', function() {
     var done = this.async();
 
     exec('apm link .', function(err, stdout, stderr) {
-      afterExec(err, stdout, stderr, done);
+      done(afterExec(err, stdout, stderr));
     });
   });
 
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
     var done = this.async();
 
     exec('apm test', function(err, stdout, stderr) {
-      afterExec(err, stdout, stderr, done);
+      done(afterExec(err, stdout, stderr));
     });
   });
 
@@ -38,7 +38,7 @@ module.exports = function (grunt) {
     var done = this.async();
 
     exec('apm unlink .', function(err, stdout, stderr) {
-      afterExec(err, stdout, stderr, done);
+      done(afterExec(err, stdout, stderr));
     });
   });
 
